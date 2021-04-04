1033d0f2b3fabe541358a48ad2f6802400de1f11
Hoàng Việt Dũng <dung.hv194254@sis.hust.edu.vn>
2021-04-05 03:21:29 +0700

    Final test

--- src/com/kamiyuri/Main.java
+++ src/com/kamiyuri/Main.java
@@ -2,8 +2,7 @@ package com.kamiyuri;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.print("Test times 222");
-        System.out.print("sasa");
-        System.out.print("Test branch");
+        System.out.print("Hello world!");
     }
+
 }
