bfbca1f6f925bab3a449157efdcbda0bebeb3dc3
Hoàng Việt Dũng <dung.hv194254@sis.hust.edu.vn>
2021-04-05 02:14:21 +0700

    Test branch

--- src/com/kamiyuri/Main.java
+++ src/com/kamiyuri/Main.java
@@ -4,5 +4,6 @@ public class Main {
     public static void main(String[] args) {
         System.out.print("Test times 222");
         System.out.print("sasa");
+        System.out.print("Test branch");
     }
 }
