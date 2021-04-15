25a8d57f72d18b036ab266ecc1c2cf2a0acbc683
KamiYuri <hvdung28@gmail.com>
2021-04-15 10:15:18 +0700

    Lap2 commit

--- src/sample/SubScene/RecipeInf/RecipeInfomationController.java
+++ src/sample/SubScene/RecipeInf/RecipeInfomationController.java
@@ -1,4 +1,27 @@
 package sample.SubScene.RecipeInf;
 
-public class RecipeInfomationController {
+import javafx.application.Application;
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
+import javafx.scene.control.Label;
+import javafx.scene.control.TextField;
+import javafx.scene.layout.Pane;
+import javafx.stage.Stage;
+
+public class RecipeInfomationController extends Application {
+    @FXML public Stage thisStage;
+    @FXML public Label name;
+    @FXML public TextField nameText;
+
+    @Override
+    public void start(Stage thisStage) {
+        FXMLLoader loader = new FXMLLoader();
+        loader.setLocation(getClass().getResource(""));
+
+
+
+        thisStage.setScene(getClass().gets);
+        thisStage.setTitle("Recipe Information");
+    }
 }
