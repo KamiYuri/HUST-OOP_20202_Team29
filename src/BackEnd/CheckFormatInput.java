121d95429a94b7457e096b9533bf9a42a3d51710
NVTUNG <tung.nv1610@outlook.com>
2021-05-14 21:22:40 +0700

    Bo sung class kiem tra du lieu dau vao(se hoan thien them).

---
+++ src/BackEnd/CheckFormatInput.java
@@ -0,0 +1,30 @@
+package BackEnd;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+public class CheckFormatInput {
+    public static  boolean checkPhoneNumber(String phoneNumber )
+        {
+            String regex = "^0\\d{9,10}$";  // Dinh dang so dien thoai bat dau voi 0 va có 10 hoac 11 chu so
+            Pattern pattern = Pattern.compile(regex);
+            Matcher matcher = pattern.matcher(phoneNumber);
+            if(matcher.find()) {
+                return true;
+            }else {
+                return false;
+            }
+
+        }
+
+    public static boolean isFullname(String name) {
+        String regex = "^[a-zA-Z\\s]+";
+        Pattern pattern = Pattern.compile(regex);
+        Matcher matcher = pattern.matcher(name);
+        if(matcher.find()){
+            return true;
+        }else{
+            return false;
+        }
+
+    }
+}
